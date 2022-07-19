d466b05fd5a7b0b9aeef28abca04da5ceb39421b
dylst <cdylan127@gmail.com>
2022-07-19 10:26:14 -0700

    Complete the Home page
    Finished implementing the ability to filter results depending on the user clicked body part

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -22,8 +22,31 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     window.scrollTo({ top: 1800, behavior: 'smooth' });
   };
 
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === 'all') {
+        exercisesData = await fetchData(
+          'https://exercisedb.p.rapidapi.com/exercises',
+          exerciseOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+      }
+
+      setExercises(exercisesData);
+      setCurrentPage(1);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart, setExercises]);
+
   return (
-    <Box id='exercises' sx={{ mt: { lg: '110px' }, mt: '50px', p: '20px' }}>
+    <Box id='exercises' sx={{ mt: { lg: '110px', xs: '50px' }, p: '20px' }}>
       <Typography variant='h3' mb='46px'>
         Showing Results
       </Typography>

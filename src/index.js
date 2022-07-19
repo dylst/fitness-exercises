a6d79643ee4ca913bb1855ff618eb56b73aead89
dylst <cdylan127@gmail.com>
2022-07-19 15:34:26 -0700

    Fix scrolling issues
    Fixed and implemented smooth scrolling where it was needed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import ScrollToTop from './utils/scrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );

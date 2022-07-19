4e7f37f342e48f83610df65d3b88e8c815ec0c8c
dylst <cdylan127@gmail.com>
2022-07-19 12:00:07 -0700

    Display related exercise videos
    Implemented the functionality to be able to watch videos related to the selected exercise.
    This is done using the Youtube Search and Download API.
    Every displayed video includes a thumbnail, title, and channel name. When clicked, a new tab is opened to watch the corresponding video on YouTube.

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -30,11 +30,13 @@ const Detail = ({ exerciseDetail }) => {
     >
       <img src={gifUrl} alt={name} loading='lazy' className='detail-image' />
       <Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
-        <Typography variant='h3'>{name}</Typography>
+        <Typography variant='h3' textTransform='capitalize'>
+          {name}
+        </Typography>
         <Typography variant='h6'>
           Exercises keep you strong.{' '}
-          <span style={{ textTransform: 'capitalize' }}>{name}</span> bup is one
-          of the best <br /> exercises to target your {target}. It will help you
+          <span style={{ textTransform: 'capitalize' }}>{name}</span> is one of
+          the best <br /> exercises to target your {target}. It will help you
           improve your <br /> mood and gain energy.
         </Typography>
         {extraDetail.map((item) => (

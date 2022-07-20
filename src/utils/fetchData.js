cf2be7b58db56ae054f85426f7223e627c70549f
dylst <cdylan127@gmail.com>
2022-07-20 15:47:45 -0700

    Update API key

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY_2,
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
   },
 };
